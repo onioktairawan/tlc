1425f06ce5f148ab37b237ce1559e54004ddfce3
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 13:22:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from discord_listener import run_discord
 from telegram_bot import run_telegram
 
 async def main():
-    loop = asyncio.get_event_loop()
-    await asyncio.gather(
-        loop.run_in_executor(None, run_discord),
-        loop.run_in_executor(None, run_telegram)
-    )
+    await asyncio.gather(run_discord(), run_telegram())
 
 if __name__ == "__main__":
     asyncio.run(main())
