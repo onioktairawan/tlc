5b53bc43a7d8230b852a399253a125499de2ad9d
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 12:56:16 +0700

    Update discord_listener.py

--- discord_listener.py
+++ discord_listener.py
@@ -1,3 +1,4 @@
+from discord.ext import commands
 import discord
 import logging
 from dotenv import load_dotenv
@@ -12,7 +13,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="!", self_bot=True, intents=intents)
 
 @client.event
 async def on_ready():
@@ -20,6 +21,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # Optional filter kalau mau skip bot:
     if message.author.bot:
         return
 
