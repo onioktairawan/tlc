d0ed5448627935fbb33b23114b378743d172cb74
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 13:15:15 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,15 @@
-import os
 from pymongo import MongoClient
+from os import getenv
 from dotenv import load_dotenv
-import logging
 
 load_dotenv()
+client = MongoClient(getenv("MONGO_URI"))
+db = client['bridge_db']
+collection = db['message_links']
 
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["bridge_db"]
-collection = db["message_links"]
+def save_message(discord_id, telegram_msg_id):
+    collection.insert_one({"discord_id": discord_id, "telegram_msg_id": telegram_msg_id})
 
-logging.basicConfig(level=logging.DEBUG)
-
-def save_message(discord_id, telegram_id, channel_id):
-    logging.debug(f"Menyimpan link pesan: Discord {discord_id} <=> Telegram {telegram_id}")
-    collection.insert_one({
-        "discord_id": discord_id,
-        "telegram_id": telegram_id,
-        "channel_id": channel_id
-    })
-
-def get_discord_message_id(telegram_id):
-    doc = collection.find_one({"telegram_id": telegram_id})
-    return doc["discord_id"] if doc else None
+def get_discord_id(telegram_msg_id):
+    doc = collection.find_one({"telegram_msg_id": telegram_msg_id})
+    return doc['discord_id'] if doc else None
