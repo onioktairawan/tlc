42283634ca35acb3d012b711262e482728113ab4
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 13:26:48 +0700

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,38 +1,20 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CallbackQueryHandler, MessageHandler, filters, ContextTypes
-from dotenv import load_dotenv
 import os
+from telegram.ext import Application, MessageHandler, filters, CallbackQueryHandler
+from telegram import Update
+from telegram.ext import ContextTypes
+from dotenv import load_dotenv
 from bridge import send_to_discord
-from database import get_discord_id
 
 load_dotenv()
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = int(os.getenv("TELEGRAM_CHAT_ID"))
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+tg_bot = Application.builder().token(TELEGRAM_TOKEN).build()
 
-pending_replies = {}
-
-async def handle_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    tg_msg_id = update.message.reply_to_message.message_id
-    original_discord_id = get_discord_id(tg_msg_id)
-    if original_discord_id:
-        await send_to_discord(update.message.text, original_discord_id)
-
-async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    data = query.data
-    if data.startswith("reply:"):
-        original_id = data.split(":")[1]
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Balas pesan ini untuk mengirim ke Discord.",
-            reply_to_message_id=query.message.message_id,
-        )
-        pending_replies[update.effective_chat.id] = original_id
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat.id == int(TELEGRAM_CHAT_ID):
+        await send_to_discord(update.message)
 
 def run_telegram():
-    app.add_handler(CallbackQueryHandler(button_callback))
-    app.add_handler(MessageHandler(filters.REPLY & filters.TEXT, handle_reply))
-    app.run_polling()
+    tg_bot.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+    return tg_bot.run_polling()
