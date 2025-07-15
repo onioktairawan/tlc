1747b9898ccd8f30f3b625343a893f99f6d414c1
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 13:31:31 +0700

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,8 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CallbackContext, MessageHandler, filters, CallbackQueryHandler, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CallbackContext, CallbackQueryHandler, MessageHandler, filters
 import os
 from dotenv import load_dotenv
-from handlers import handle_telegram_reply
+from handlers import handle_telegram_reply, handle_reply
 
 load_dotenv()
 
@@ -11,12 +11,5 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 app = Application.builder().token(TELEGRAM_TOKEN).build()
 
-@app.message_handler(filters.TEXT & (~filters.COMMAND))
-async def handle_reply(update: Update, context: CallbackContext):
-    if "discord_reply_to" in context.user_data:
-        from discord_listener import send_to_discord  # import di dalam fungsi untuk hindari circular import
-        reply_to_id = context.user_data.pop("discord_reply_to")
-        await send_to_discord(reply_to_id, update.message.text)
-        await update.message.reply_text("Pesan sudah dikirim ke Discord.")
-
 app.add_handler(CallbackQueryHandler(handle_telegram_reply))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_reply))
