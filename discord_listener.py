5d7d79a248614b3434fc7d1d5ddbc53774e20cf5
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 13:15:34 +0700

    Update discord_listener.py

--- discord_listener.py
+++ discord_listener.py
@@ -1,28 +1,28 @@
-from discord.ext import commands
 import discord
-import logging
+from discord import Message
 from dotenv import load_dotenv
 import os
 from bridge import send_to_telegram
 
 load_dotenv()
-logging.basicConfig(level=logging.DEBUG)
-
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-# Tidak menggunakan Intents karena discord.py-self tidak menyediakannya
-client = commands.Bot(command_prefix="!", self_bot=True, intents=None)
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    logging.debug(f"[DISCORD] Logged in as {client.user}")
+    print(f"[Discord] Logged in as {client.user}")
 
 @client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    logging.debug(f"[DISCORD] Pesan diterima dari {message.author.name}: {message.content}")
-    await send_to_telegram(message)
+async def on_message(message: Message):
+    if message.author.bot: return
+    content = message.content.strip()
+    author = str(message.author)
+    discord_msg_id = str(message.id)
+    await send_to_telegram(author, content, discord_msg_id)
 
-async def run_discord():
-    await client.start(DISCORD_TOKEN)
+def run_discord():
+    client.run(DISCORD_TOKEN, bot=False)
